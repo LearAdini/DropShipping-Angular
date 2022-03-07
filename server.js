b7e509cadf7116dc204b6aada4613f49711e6c3b
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-03-07 21:37:29 +0200

    Deployment configuration

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-
-app.use(express.static(__dirname));
-
+const app = express();
+app.use(express.static(__dirname + '/dist/drop-shipping'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/drop-shipping/index.html'));});
 app.listen(process.env.PORT || 8080);
-
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/src/index.html'));
-});
-
-console.log('Console listening on port 8080');
-
